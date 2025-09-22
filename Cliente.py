5c1fb1acf3c3a1501899bdd8904b363c8282d4e9
Pedro Sampaio <pssa@cesar.school>
2025-09-22 17:15:19 -0300

    Cliente e sevidor finalizados

--- Cliente.py
+++ Cliente.py
@@ -1,15 +1,32 @@
 import socket
-import time
-import json
+import threading
             
 
 # ==================== CLIENTE ==================== #
 
-#Criando conexão com servidor
-soquete_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-endereco_servidor = ("localhost", 1024)
-soquete_cliente.connect(endereco_servidor)
+def enviar_pedido(i):
+    #Criando conexão com servidor
+    soquete_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soquete_cliente.connect(("localhost", 1024))
 
-def threads():
-    
-soquete_cliente.close()
+    #Envia o numero do pedido ao servidor
+    soquete_cliente.sendall(f"{i}".encode())
+
+    #Recebe confirmação do servidor
+    print(soquete_cliente.recv(1024).decode())
+
+    #Fecha a conexão
+    soquete_cliente.close()
+
+
+#lista para armazenar as threads dos clientes
+clientes = []
+
+#Looping para criação de vários clientes
+for i in range(20):
+    t = threading.Thread(target=enviar_pedido, args=(i,))
+    clientes.append(t)
+    t.start()
+
+for t in clientes:
+    t.join()
--- Servidor.py
+++ Servidor.py
@@ -4,28 +4,54 @@ import time
 import queue
 
 # ==================== SERVIDOR ==================== #
-def Cliente(pedido):
-    with Lock:
-        buffer.put(pedido)
-        print("Cliente pediu um pedido: ", pedido)
-        time.sleep(2)
 
-def Cozinheiro():
-    with Lock:
-        comida = buffer.get()
-        print("Cozinheiro preparando pedido: ", comida)
+#Função do cliente (produtor)
+def Cliente(cliente, endereco):
+    with cliente:
+        #Recebe o número do pedido passado pelo cliente
+        pedido = cliente.recv(1024).decode()
+
+        if not pedido:
+            print(f"Cliente {endereco} não enviou nenhum pedido.")
+            return
+
+        with lock:
+            buffer.put(pedido)
+            print(f"Cliente {endereco} fez um pedido: {pedido}")
+            cliente.send("Seu pedido foi adicionado à fila.".encode())
+            time.sleep(2)
+
+
+
+#Função do cozinheiro (consumidor)
+def Cozinheiro(cozinheiro):
+    while True:
+        pedido = buffer.get()
+        print(f"Cozinheiro {cozinheiro} preparando pedido: {pedido}")
         time.sleep(2)
+        print(f"Cozinheiro {cozinheiro} terminou o pedido: {pedido}")
+        buffer.task_done()
         
 
+#Configurações iniciais para o projeto
+lock = threading.Lock()
+buffer = queue.Queue(maxsize=5)
 
 #Configura servidor e coloca no ar
-Lock = threading.Lock()
-buffer = queue.Queue(maxsize=5)
 soquete_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soquete_servidor.bind(('localhost', 1024))
 soquete_servidor.listen(5)
 print("Servidor local rodando na porta 1024")
 
-for i in range(20):
-    cliente = soquete_servidor.accept()
+#Criação dos cozinheiros
+for i in range(2):
+    cozinheiro = i+1
+    threading.Thread(target=Cozinheiro, args=(cozinheiro,), daemon=True).start()
 
+#Loop para aceitação de clientes
+while True:
+    #aceita conexão com cliente
+    cliente, endereco = soquete_servidor.accept()
+
+    #Inicia uma thread para o cliente
+    threading.Thread(target=Cliente, args=(cliente, endereco), daemon=True).start()
